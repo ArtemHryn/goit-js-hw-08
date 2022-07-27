850d0526533c945802583027cd06c1e5e5b7c0d0
ArtemHryn <grynbest@gmail.com>
2022-07-27 17:54:08 +0300

    finished HW8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,46 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe, { muted: true, autoplay: true });
+
+onFinishedTime();
+
+player.on('timeupdate', throttle(onSaveTime, 1000));
+player.on('ended', onClearStorage);
+
+function onClearStorage() {
+  localStorage.removeItem(CURRENT_TIME);
+}
+
+function onSaveTime() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem(CURRENT_TIME, seconds);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function onFinishedTime() {
+  player
+    .setCurrentTime(localStorage.getItem(CURRENT_TIME))
+      .then(function (seconds) {
+        console.log("The video started on", seconds);
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          console.log('Some Error here');
+          break;
+
+        default:
+          console.log('Some Error');
+          break;
+      }
+    });
+}
